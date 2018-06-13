8b95595cecb81dc6e78870ca902f9f1c9666fcfb
Shuaiwen <33140647+Shuaiwenxiaohong1314@users.noreply.github.com>
2018-06-13 15:37:16 +0800

    Update spider.py

--- spider.py
+++ spider.py
@@ -1,1 +1,18 @@
-okokanyway
+#okokanyway
+#简单爬虫
+import urllib2
+def download(url)：
+    return urllib2.urlopen(url).read()
+    
+#下面是另一段
+def download(url,num_retries=2)：
+    print("Doenloading",url)
+    try：
+        html=urllib2.urlopen(url).read()
+    except urllib2.URLError as e：
+        print ("Download error:",e.reason)
+        html=none
+        if num_retries>0:
+            if hasattr (e,'code') and 500 <= e.code <600:
+            return download(url,num_retries-1)
+    return html
